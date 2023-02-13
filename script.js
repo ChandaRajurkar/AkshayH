d65aecb47f2bf1ced0e1b9a6b6e9e15c02345b41
ChandaRajurkar <89902385+ChandaRajurkar@users.noreply.github.com>
2023-02-13 13:41:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+document.getElementById("offer1").addEventListener("click", function() {
+  var x = document.getElementById("offerpara1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    //document.getElementById("offerpara2").style.display = "none";
+    document.getElementById("icondown1").style.display = "none";
+    document.getElementById("iconup1").style.display = "block";
+    
+  
+  } else {
+    x.style.display = "none";
+    //document.getElementById("hr").style.display = "none";
+    document.getElementById("icondown1").style.display = "block";
+    document.getElementById("iconup1").style.display = "none";
+  }
+});
+
+
+document.getElementById("offer2").addEventListener("click", function() {
+  var y = document.getElementById("offerpara2");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+    //document.getElementById("offerpara1").style.display = "none";
+    document.getElementById("icondown2").style.display = "none";
+    document.getElementById("iconup2").style.display = "block";
+  } else {
+    y.style.display = "none";
+    document.getElementById("icondown2").style.display = "block";
+    document.getElementById("iconup2").style.display = "none";
+  }
+});
+
+
+
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    var name = $("input[name='name']").val();
+    var email = $("input[name='email']").val();
+    var message = $("textarea[name='message']").val();
+
+    if (!name || !email || !message) {
+      alert("All fields are required.");
+      return;
+    }
+
+    if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+      alert("Invalid email address.");
+      return;
+    }
+
+    var formData = {
+      name: name,
+      email: email,
+      message: message
+    };
+
+    $.ajax({
+      type: "POST",
+      url: "form-processor.php",
+      data: formData,
+      success: function() {
+        alert("Form submitted successfully.");
+      }
+    });
+  });
+});
